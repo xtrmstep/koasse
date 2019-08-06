3afc36ea18d93255cc41369895ff8afe792c715f
Alexander Goida <xtrmstep@users.noreply.github.com>
2019-08-07 02:07:57 +0300

    Added protobuf only to koa

--- webchat.api/src/middleware/home.ts
+++ webchat.api/src/middleware/home.ts
@@ -1,11 +1,15 @@
 import Router from 'koa-router';
+import { protobufParser, protobufSender } from 'koa-protobuf';
+import messages from '../protos/bundle.js';
 
 const router = new Router();
 
 export default () => {
     router
         .get('/', async (ctx) => {
-            ctx.body = "alive!";
+            ctx.proto = messages.SampleMessage.create({
+                example_field: 'alive!'
+            });
         });
     return router.middleware();
 };
